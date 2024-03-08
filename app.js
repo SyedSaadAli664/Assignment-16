ab98de16bc1ba45e1e36c1cd273ccbaf204db99f
Syed Saad Ali <syedsaadali664@gmail.com>
2024-03-09 01:37:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+alert("Welcome to my website");
+
+var firstName = prompt("Please Enter your First name:");
+var lastName = prompt("Please Enter your Last name:");
+var age = prompt("Please Enter your age: ( Only Numbers )");
+var email = prompt("Please Enter your email address: ")
+
+console.log("Hi " + firstName + ", nice to meet you.");
+console.log("This is the data you entered: ");
+console.log("Your full name is: " + firstName + " " + lastName);
+console.log("Your age is: " + age);
+console.log("Your email is: " + email);
